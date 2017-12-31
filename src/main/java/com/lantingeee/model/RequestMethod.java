d5bef91f7088a42b5ff3ad6b504d5d4293fc5e05
lantingeee <lantingeee@gmail.com>
2017-12-31 22:54:54 +0800

    create RequestFactory and ParameterFactory

---
+++ src/main/java/com/lantingeee/model/RequestMethod.java
@@ -0,0 +1,7 @@
+package com.lantingeee.model;
+
+/**
+ * Created by lantingeee on 29/12/2017.
+ */
+public interface RequestMethod {
+}
---
+++ src/main/java/example/SimpleController.java
@@ -0,0 +1,7 @@
+package example;
+
+/**
+ * Created by lantingeee on 15/12/2017.
+ */
+public class SimpleController {
+}
